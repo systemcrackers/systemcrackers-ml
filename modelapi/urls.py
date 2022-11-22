287649afc4e087e568b7123b7a3f319ebabc80c8
blackchapel <kc@protonmail.com>
2022-11-22 19:18:50 +0530

    changed folder name

--- modelapi/urls.py
+++ modelapi/urls.py
@@ -3,5 +3,5 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('model/', views.ModelApi.as_view(), name="model" ),
+    path('model/dyslexia', views.ModelApi.as_view(), name="model" ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
